dc6eee511d738b139d1f13d102f435875e126f80
hour_panha <134907496+hour-panha23@users.noreply.github.com>
2025-11-23 18:09:38 +0700

    Updates

---
+++ _next/static/hNyjdrwXNrXhI48IimGKw/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/love-hub//_next/:path+",
+        "destination": "/love-hub/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
